3c78c073e51dd64cfe4c66cf0d26fdad52d0657b
predu <3pedrolucca@gmail.com>
2025-09-08 00:32:53 -0300

    lesson5 watch and add lesson6 - Functions

--- dist/lesson4.js
+++ dist/lesson4.js
@@ -34,7 +34,23 @@ type Guitarist = {
 
 quando devo usar qual?
 
-interface é usada quando pensamos em uma 'classe'
+interface é usada quando pensamos em uma 'classe'. Elas são mais usadas para descrever o contrato de classes e objetos
+    - Cenários como criação de bibliotecas
+    - Extensão de interfaces, ótimo para estender uma interface de outra => criando hierarquias de tipos.
+    - Declaração de múltiplas partes
+
+type é mais usado para um geral. É mais versátil e podem ser usadas para criar aliases de tipos primitivos, uniões, interseções e tuplas
+
+Característica	                            interface	type
+Definição de Objeto	                            ✔️	     ✔️
+Aliases de Tipos Primitivos	                    ❌	    ✔️
+União de Tipos (|)	                            ❌	    ✔️
+Interseção de Tipos (&)	                        ❌	    ✔️
+Declaração por Múltiplas Partes (mesclar)	    ✔️	     ❌
+Extensão	Usa extends	Usa & ou `
+
+Exportar para as Planilhas
+
 
 */
 let evh = {
@@ -48,8 +64,35 @@ let jp = {
     albums: ['I', 'II', 'IV'],
 };
 const greetGuitarist = (guitarrist) => {
-    return `Hello ${guitarrist.name}!`;
+    return `Hello ${guitarrist.name?.toUpperCase}!`;
 };
+/*
+No return, para que não ocorra do objeto ser possivelmente indefinido, o TypeScript coloca o '?', se não quisermos que seja dessa forma, podemos fazer a verifição manual, chamado de estreitamento.
+
+const greetGuitarist = (guitarrist: Guitarist) => {
+    if(guitarist.name){
+        return `Hello ${guitarrist.name.toUpperCase}!`
+    }
+    return 'Hello!'
+}
+
+basicamente essa função vai verificar, se o guitarrist.name existe, ou seja, se não é null nem undefined. Caso não possua ele vai retornar apenas o 'Hello!'
+
+No caso anterior, o TypeScript só vai alterar para "toUpperCase" (Letra maiuscula), caso a propiedade tiver um valor.
+
+
+*/
 console.log(greetGuitarist(jp));
+// Enums
+// Algo adicionado na linguagem durante o tempo de execução
+var Grade;
+(function (Grade) {
+    Grade[Grade["U"] = 1] = "U";
+    Grade[Grade["D"] = 2] = "D";
+    Grade[Grade["C"] = 3] = "C";
+    Grade[Grade["B"] = 4] = "B";
+    Grade[Grade["A"] = 5] = "A";
+})(Grade || (Grade = {}));
+console.log(Grade.U);
 export {};
 //# sourceMappingURL=lesson4.js.map
---
+++ dist/lesson5.js
@@ -0,0 +1,6 @@
+// Type Aliases
+// Literal types
+let myName;
+let userName;
+export {};
+//# sourceMappingURL=lesson5.js.map
